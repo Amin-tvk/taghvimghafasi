4a35872370aa1e0a296a085bc2db662867069d1a
Amin-tvk <tavakolia088@gmail.com>
2023-06-28 17:14:13 +0330

    complete :)

--- index.js
+++ index.js
@@ -4,22 +4,24 @@ const { FileFlavor, hydrateFiles } = require ("@grammyjs/files");
 const fs = require('fs');
 const moment = require('moment-timezone');
 
-const bot = new Bot('5441085680:AAGiFH4_oMoCd0dtBao7HB7r6MN-PvLB1Ww');
+const bot = new Bot('5992329338:AAFeMzENjcxAATji5mpDcgLNZg7VjfFfv9U');
 bot.api.config.use(hydrateFiles(bot.token));
 const timezone = 'Asia/Tehran';
 moment.tz.setDefault(timezone);
-
+ const channel_id = 'taghvimghafasi'
+ 
 
 bot.on([":video", ":animation" ], async (ctx) => {
   console.log(ctx.chat.username)
+  
   const tehranTime = moment().format('HH:mm');
-  if ( ctx.chat.username == 'dsffewf' && tehranTime == '00:00') {
+  if ( ctx.chat.username == channel_id && tehranTime == '00:00') {
     const file = await ctx.getFile();
   // Download the file to a temporary location.
   const path = await file.download("./src/test.mp4");
   // Print the file path.
   console.log("File saved at ", path);
-  await ctx.reply('این ربات تنها در گروه‌ها فعال است.' );
+  
   }else{
     console.log(" در این کانال کار نمیکند!!")
   }
@@ -27,40 +29,26 @@ bot.on([":video", ":animation" ], async (ctx) => {
 });
 bot.on('message', async (ctx) => {
   const chatType = ctx.chat.type;
+  let currentEpoch1 = Math.floor(new Date().getTime()/1000.0)
   console.log(chatType)
   if (chatType === 'private') {
     await ctx.reply('این ربات تنها در گروه‌ها فعال است.');
-  } else if (chatType === 'group' || chatType === 'supergroup') {
-    const message = ctx.message;
+  } else if (chatType === 'group' || chatType === 'supergroup'  ) {
+    dailymessage =  currentEpoch1 - ctx.message.date
+    console.log(dailymessage) 
+    console.log(ctx.message.date)
+    console.log(currentEpoch1)
+    if ( dailymessage <= '30' ) {
+      const message = ctx.message;
     const text = message.text || '';
     const member = message.from;
-    
-
-    
-// حالا می‌توانید فایل را دانلود کنید با استفاده از کتابخانه‌های مناسب در نود جی اس
-
-    
+    console.log(message.date) 
     if (text.includes('امروز چندمه')) {
       const memberName = member.first_name || member.username;
-      
       const chatId = '@dsffewf';
-      /*
-      const messages = await ctx.telegram.getChatMessages(chatId);
-      
-      let latestGifMessage;
-for (const message of messages) {
-  if (message.animation && message.animation.mime_type === 'image/gif') {
-    latestGifMessage = message;
-    break;
-  }
-}
-*/
       const logo = "https://ibb.co/hfnLP3w";
       const photoName = 'photo-2022-12-27-01-27-50.jpg';
-      //  const photoPath = path.join(__dirname, photoName);
-      //await ctx.replyWithPhoto(photoPath, { caption: 'متن توضیحات عکس' });
       const animationPath = './giphy.gif';
-      //await ctx.replyWithAnimation(animationPath, { caption: 'متن توضیحات گیف' });
       const photoPath = './src/test.mp4';
       const inputFile = new InputFile(photoPath);
 
@@ -68,16 +56,10 @@ for (const message of messages) {
         force_reply: true,
         reply_to_message_id: ctx.msg.message_id,
       } );
-      /*await ctx.reply(` ${memberName}!` ,
-      
-      
-      {
-        force_reply: true,
-        reply_to_message_id: ctx.msg.message_id,
-      },
-      );*/
       console.log("send gif" , ctx.msg.message_id)
     }
+    }
+    
   } 
   else if(chatType === 'CHANNEL'){
     
@@ -85,6 +67,8 @@ for (const message of messages) {
   }
   
 });
-
+const currentEpoch = new Date().getTime();
 bot.start();
+
+console.log(Math.floor(new Date().getTime()/1000.0) )
 
